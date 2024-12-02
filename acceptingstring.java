368a4734165721f498ffb4bfd752ec37e3cac5f6
sad54545675 <tayyibafathima666@gmail.com>
2024-12-02 15:39:15 +0530

    Add files via upload

---
+++ acceptingstring.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class acceptingstring{
+    public static void main(String[] args){
+        //accepting a string
+        Scanner sc = new Scanner(System.in);
+        String a=sc.next();
+        String b=sc.next();
+        String c=sc.next();
+        System.out.println(b);
+        System.out.println(c);                     
+    }
+
+}
