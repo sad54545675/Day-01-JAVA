6a3d92cf6825bf6ad5c3fe1e22fd99dc23bab180
sad54545675 <tayyibafathima666@gmail.com>
2024-12-02 16:04:01 +0530

    Add files via upload

---
+++ str.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class str{
+    public static void main(String[] args){
+        //Accepting string
+        Scanner sc = new Scanner(System.in);
+        String a=sc.next();
+        System.out.println("Congratulations "+a+" you won 5 lakhs");
+    }
+}
