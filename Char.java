0046a1ab52f7b89b90624a7cd445b2068c9b7f5d
sad54545675 <tayyibafathima666@gmail.com>
2024-12-02 15:03:54 +0530

    Add files via upload

---
+++ Char.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Char{
+    public static void main(String[] args){
+        //conversion of ascii value to char
+        int a=50;
+        char b=(char)(a);
+        char c='7';
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
