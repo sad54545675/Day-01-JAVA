05618b0f05dc4b23deee5feba98277fc49aa9a30
sad54545675 <tayyibafathima666@gmail.com>
2024-12-02 15:19:33 +0530

    Add files via upload

---
+++ printingnextchar.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class printingnextchar{
+    public static void main(String[] args){
+        //conversion of ascii value to char
+        char c='a';
+        int b=c;
+        b+=1;
+        char v=(char)(b);
+        System.out.println(v);
+    }
+}
